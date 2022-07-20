603cddf6a09f9a1cd7f142a77cc930538f93e245
fizik1 <shahobnarpay@gmail.com>
2022-07-20 19:09:46 +0500

    first

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+This is a echo bot.
+It echoes any incoming text messages.
+"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from transliterate import to_cyrillic, to_latin
+
+API_TOKEN = '5416974893:AAGp0vJnX2qRaQmSeXnd12HT3SkGYZAFXwY'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    res = 'Assalomu alaykum, bu lotin-kiril o\'giruvchi boti'
+    res+= '\nIstalgan so\'zni kiriting'
+    await message.reply(res)
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+    print(message)
+    msg = message.text
+    print(msg.isascii)
+    if msg.isascii():
+        response = to_cyrillic(msg)
+        await message.answer(response)
+    else:
+        response = to_latin(msg)
+        await message.answer(response)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
